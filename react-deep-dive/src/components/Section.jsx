aab9e86d70e5717f99825f170057f0d68f5c92ea
Abhishek <abhishektiwarimrt@users.noreply.github.com>
2024-06-03 19:35:02 +0530

    Forwaded Props to Inner HTML

---
+++ react-deep-dive/src/components/Section.jsx
@@ -0,0 +1,10 @@
+import { Children } from "react";
+
+export default function Section({ title, children, id }) {
+  return (
+    <section id={id}>
+      <h2>{title}</h2>
+      {children}
+    </section>
+  );
+}
